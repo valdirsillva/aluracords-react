6ca511158973843585f12241124de14cf9bd3d54
Valdir Silva <valdirpiresba@gmail.com>
2022-01-27 01:28:59 -0300

    enviando mensagens

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d44aba51d6e54f8"; }
+/******/ 		__webpack_require__.h = function() { return "da5c4c196fbf3fc2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
